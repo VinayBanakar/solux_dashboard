509df4b7468ace6fa9c49ea91fca90cac82b1794
SujaySKumar <sujay.skumar141295@gmail.com>
2017-06-11 23:29:03 +0530

    Iteration 1 of solux with dummy data

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -111,23 +111,37 @@ demo = {
     },
 
     initGoogleMaps: function(){
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+        var locations = [
+            ['Ram, ID: 232, Nagarbhavi', 12.959945, 77.508283, 4],
+            ['Varma, ID: 324, Koramangala', 12.927923, 77.627108, 5],
+            ['Geetha, ID: 432, ITPL', 12.986077, 77.735907, 3],
+            ['Vijay, ID: 123, Majestic', 12.976664, 77.571256, 2],
+            ['Neel, ID: 343, Indiranagar', 12.971891, 77.641154, 1],
+            ['Akash, ID: 522, Banashankari', 12.925453, 77.546757]
+            ];
         var mapOptions = {
-          zoom: 13,
-          center: myLatlng,
+          zoom: 12,
+          center: new google.maps.LatLng(12.97, 77.59),
           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
           styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
 
         }
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        for (i = 0; i < locations.length; i++) { 
+           marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+          });
 
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(locations[i][0]);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+      }
     },
 
 	showNotification: function(from, align){
